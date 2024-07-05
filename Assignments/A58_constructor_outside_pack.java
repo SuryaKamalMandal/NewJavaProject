98976ca91a6315513fe117b4c4c282df1faa8d3c
Surya Kamal Mandal <48994622+SuryaKamalMandal@users.noreply.github.com>
2024-07-05 17:59:04 +0530

    Java Program

--- Assignments/A58_constructor_outside_pack.java
+++ Assignments/A58_constructor_outside_pack.java
@@ -4,7 +4,8 @@ import JavaPro.A58_outside_pack_constructor;
 public class A58_constructor_outside_pack {
 
 	public static void main(String[] args) {
-		
+
+		// the constructor are in JavaPro Package, class name is A58_outside_pack_constructor
 		new A58_outside_pack_constructor(99);
 	}
 }
