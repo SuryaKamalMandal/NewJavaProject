84c0106309247910eaa33f80019e2b9e57815c66
Surya Kamal Mandal <48994622+SuryaKamalMandal@users.noreply.github.com>
2024-07-05 17:58:27 +0530

    Java Program

--- Assignments/A53_outside_package_subclass.java
+++ Assignments/A53_outside_package_subclass.java
@@ -6,6 +6,7 @@ public class A53_outside_package_subclass extends A53_outsidepackage {
 	public static void main(String[] args) {
 		
 		A53_outside_package_subclass as= new A53_outside_package_subclass();
+		// the methods are in JavaPro Package, class name is A53_outsidepackage
 		as.add();
 		as.multi();
 	}
