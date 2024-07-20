b28407d1d0440c09b051abd97a58302948ae3fb3
Surya Kamal Mandal <48994622+SuryaKamalMandal@users.noreply.github.com>
2024-07-20 11:03:23 +0530

    Java Program

---
+++ Selenium/A134_amazon.java
@@ -0,0 +1,17 @@
+package Selenium;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class A134_amazon {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		ChromeDriver driver= new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
+		driver.findElement(By.partialLinkText("Customer")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Mobiles")).click();
+
+	}
+}
---
+++ Selenium/A142_amazon_dropdown.java
@@ -0,0 +1,20 @@
+package Selenium;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class A142_amazon_dropdown {
+
+	public static void main(String[] args) {
+		
+		ChromeDriver driver= new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
+		WebElement drop= driver.findElement(By.xpath("//select[@id='searchDropdownBox']"));
+		
+		Select s=new Select(drop);
+		s.selectByVisibleText("Books");
+	}
+}
