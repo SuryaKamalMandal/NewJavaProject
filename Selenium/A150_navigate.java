73144827bb5c94b8d8a54a83e457881427e4a4ca
Surya Kamal Mandal <48994622+SuryaKamalMandal@users.noreply.github.com>
2024-07-23 15:43:42 +0530

    Java Program

---
+++ Selenium/A150_navigate.java
@@ -0,0 +1,18 @@
+package Selenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class A150_navigate {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		ChromeDriver driver= new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		driver.navigate().back();
+		Thread.sleep(2000);
+		driver.navigate().forward();
+		Thread.sleep(2000);
+		driver.navigate().refresh();
+	}
+}
